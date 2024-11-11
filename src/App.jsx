1f8058e48168ce9c4665435553778d99fb689c75
Lucas Fiusa <lucasfiusa100@gmail.com>
2024-11-11 15:57:24 -0300

    calculadora

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import "./style.scss"
 
 export default function App() {
   //VARIAVEL DE ESTADO
@@ -6,8 +7,11 @@ export default function App() {
   return (
     <main>
       <h1>Calculadora</h1>
+      
       <input type="number" placeholder="Insira um número"/>
+
       <input type="number" placeholder="Insira um número"/>
+
       <div>
         <button>+</button>
         <button>-</button>
