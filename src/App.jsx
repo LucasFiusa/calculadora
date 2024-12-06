cb3927853df2f809481fe9d63a95a25254e4d972
LucasFiusa <lucasfiusa100@gmail.com>
2024-12-06 00:39:54 -0300

    finalização

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,24 @@ export default function App() {
     setSegundoValor(Number(e.target.value))
   }
 
+  const [resultado, setResultado] = useState();
+
+  const soma = () => {
+    setResultado(primeiroValor + segundoValor);
+  };
+
+  const subtracao = () => {
+    setResultado(primeiroValor - segundoValor);
+  };
+  
+  const divisao = () => {
+    setResultado(primeiroValor / segundoValor);
+  };
+
+  const multiplicacao = () => {
+    setResultado(primeiroValor * segundoValor);
+  };
+
   return (
     <main>
       <h1>Calculadora</h1>
@@ -26,12 +44,12 @@ export default function App() {
       <input onChange={capturandoSegundoValor} type="number" placeholder="Insira um número"/>
 
       <div>
-        <button>+</button>
-        <button>-</button>
-        <button>÷</button>
-        <button>x</button>
+        <button onClick={soma}>+</button>
+        <button onClick={subtracao}>-</button>
+        <button onClick={divisao}>÷</button>
+        <button onClick={multiplicacao}>x</button>
       </div>
-      <h3></h3>
+      <h3>{resultado}</h3>
     </main>
   )
 }
